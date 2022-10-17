df34818047b011976e06faba04256356c815eafe
eduardobarbosa-ti <eduardobb@rede.ulbra.br>
2022-10-16 22:38:03 -0300

    modified: rename in files SQL

--- 2022/fundamentos_banco_de_dados/aula02_tde/tde.sql
+++ 2022/fundamentos_banco_de_dados/aula02_tde/tde.sql
@@ -4,7 +4,7 @@ create table categoria(
 );
 
 create table produtos(
-    id_categoria int auto_increment primary key,
+    id_categoria int primary key,
     descricao varchar(200),
     data_cadastro date not null,
     valor_unitario float not null,
@@ -63,15 +63,16 @@ ALTER TABLE fornecedor MODIFY nome varchar(130);
 
 /*8 - Excluindo tabela com restrição de chave estrangeira*/
 DROP TABLE fornecedor;
-MySqlError { ERRO 1451 (23000): Não é possível excluir ou atualizar uma linha pai: falha de restrição de chave estrangeira }
-
+/*
+MySqlError { ERROR 1451 (23000): Cannot delete or update a parent row: a foreign key constraint fails }
 O sgbd não autorizou a exclusão da tabela por causa da declaração de restrição na foreign key "on delete restrict".
+*/
 
 /*9 - Adicionando nova coluna em tabela*/
 
 alter table pedidos_produtos_fornecedor add column numero_do_pedido varchar(20) not null unique;
 
-/*10 - */
+/*10 - Populando dados nas tabelas*/
 
 insert into categoria(nome) 
 values
@@ -81,3 +82,43 @@ values
 ('pets'),
 ('carnes');
 
+insert into produtos(id_categoria, descricao, valor_unitario) 
+values
+('1','produto top','7.50'),
+('2','produto bom','3.50'),
+('3','produto maravilhoso','50.50'),
+('4','produto ruim','0.50')
+
+insert into fornecedor(nome, data_ultima_compra) 
+values
+('André','2022-11-02'),
+('Paulo','2022-11-07'),
+('Zé','2022-11-22'),
+('Jocenir','2022-11-14')
+
+insert into pedidos_produtos_fornecedor(quantidade, valor_unitario, data_pedido,numero_do_pedido, id_produto, id_fornecedor) 
+values
+('3','3.60','2022-11-02','574','1','4'),
+('5','7.90','2022-11-07','467','2','3'),
+('7','4.67','2022-11-22','233','3','2'),
+('5','3.8','2022-11-14','452','4','1')
+
+
+/*11 - Deletando uma tupla da tabela*/
+ DELETE FROM produtos 
+ WHERE 1; 
+/*
+MySqlError { ERROR 1451 (23000): Cannot delete or update a parent row: a foreign key constraint fails (`db272289`.`pedidos_produtos_fornecedor`, CONSTRAINT `produtos_fk_pedidos_produtos_fornecedor` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id_categoria`) ON UPDATE CASCADE) }
+
+o sgbd não autoriza pela declaração de restrição da foreign key "on delete restrict"
+*/
+
+/*12 - Alterando chave primaria da tupla de uma tabela*/
+
+UPDATE produtos
+SET id_categoria = '5'
+WHERE id_categoria = 1;
+
+/*
+o sgbd aceitou a alteração porém alterou em cascata todos os id_categoria do produto. Pois declaramos na restrição da foreign key "on update cascate"
+*/
