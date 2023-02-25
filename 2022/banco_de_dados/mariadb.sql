f6e50aa02bbdd14ef7849cbbc01b957d1bc36b07
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-02-25 15:48:23 -0300

    Criando o DataBase e as tables do Banco de dados

--- 2022/banco_de_dados/mariadb.sql
+++ 2022/banco_de_dados/mariadb.sql
@@ -13,3 +13,47 @@ CREATE TABLE Produtos(
     nomeCategoria varchar(20) not null,
     descricaoCategoria varchar(50) not null
 );
+
+CREATE TABLE Filial(
+	codFilial int primary key auto_increment,
+    nome varchar(100) not null,
+    endereco varchar(100) not null,
+    codCidade int not null,
+    constraint cidade_fk_filial
+    foreign key(codCidade) references Cidades(codcid)
+		on delete restrict
+        on update cascade
+);
+
+CREATE TABLE Empregados(
+	codEmpregado int primary key auto_increment,
+    nome varchar(100) not null,
+    endereco varchar(100) not null,
+    ct int not null,
+    rg varchar(15) not null,
+    cpf char(11) not null,
+    salario decimal(12,2) not null,
+    codCidade int not null,
+    codFilial int not null,
+    constraint cidade_fk_empregados
+    foreign key(codCidade) references Cidades(codcid)
+		on delete restrict
+        on update cascade,
+	constraint filial_fk_empregados
+	foreign key(codFilial) references Filial(codFilial)
+		on delete restrict
+        on update cascade
+);
+
+CREATE TABLE Vende(
+	codProduto int not null,
+    codFilial int not null,
+    constraint produto_fk_vende
+    foreign key(codProduto) references Produtos(codprod)
+		on delete restrict
+        on update cascade,
+	constraint filial_fk_vende
+    foreign key(codFilial) references Filial(codFilial) 
+		on delete restrict 
+        on update cascade
+);
