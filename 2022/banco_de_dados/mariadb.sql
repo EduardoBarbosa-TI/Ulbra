d04b0614b270177874eae4f1c7d9277235a7a71b
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-02-25 17:16:07 -0300

    Populando as tabelas do Banco de Dados

--- 2022/banco_de_dados/mariadb.sql
+++ 2022/banco_de_dados/mariadb.sql
@@ -30,8 +30,8 @@ CREATE TABLE Empregados(
     nome varchar(100) not null,
     endereco varchar(100) not null,
     ct int not null,
-    rg varchar(15) not null,
-    cpf char(11) not null,
+    rg varchar(15) not null unique,
+    cpf char(11) not null unique,
     salario decimal(12,2) not null,
     codCidade int not null,
     codFilial int not null,
@@ -57,3 +57,44 @@ CREATE TABLE Vende(
 		on delete restrict 
         on update cascade
 );
+
+INSERT INTO Cidades(nome,uf)
+VALUES
+	('Rio Grande do Sul', 'RS'),
+    ('São Paulo', 'SP'),
+    ('Minas Gerais', 'MG'),
+    ('Mato Grosso do Sul', 'MS'),
+    ('Espírito Santo', 'ES')
+
+INSERT INTO Produtos(preco,nomeCategoria,descricaoCategoria)
+VALUES 
+	(1.200, 'Colchões', 'Colchões ultra macios com molas'),
+    (1.000, 'Computadores', 'Computadores de última geração'),
+    (1.500, 'Tapetes', 'Pele do último mamute brasileiro'),
+    (2.000, 'Ventiladores', 'Turbina de avião acoplada'),
+    (2.200, 'Celulares', 'Celulares com 4GB Ram')
+
+INSERT INTO Filial(nome, endereco,codCidade)
+VALUES
+('f1','Rua Jaguatirica, 575',1),
+('f2','Rua Leopoldina, 465',2),
+('f3','Rua Paulo Teixeira, 425',3),
+('f4','Rua Ulbra, 135',4),
+('f5','Rua Leão Marinho, 405',5) 
+
+INSERT INTO Empregados(nome,endereco,ct,rg,cpf,salario,codCidade,codFilial)
+VALUES
+	('José','Rua Carlos Alves',39,21342456787,12345678912,2.000,5,1),
+    ('Carlos','Rua Josefina Santos',23,21546456757,12445798912,3.000,4,2),
+    ('João','Rua Vargas',32,21342856967,12352678912,5.000,3,3),
+    ('Ricardo','Rua Condado',13,21342146767,12325678912,3.400,2,4),
+    ('Raul','Rua Maria Luiza',34,1342566707,12895678912,2.500,1,5)
+
+INSERT INTO Vende(codProduto,codFilial)
+VALUES 
+	(1,5),
+    (2,4),
+    (3,3),
+    (4,2),
+    (5,1)
+--3
