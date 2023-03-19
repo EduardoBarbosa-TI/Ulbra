6267095025af9c111988de6abe8593a969ed5781
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-03-19 18:01:36 -0300

    Organizando os repositórios

---
+++ 2023/banco_de_dados/aula03/view.sql
@@ -0,0 +1,4 @@
+--Crie uma visão sobre uma tabela única incluindo todos os seus dados. Depois faça uma inserção de dados na visão. Verifique o que acontece com a tabela base.
+
+
+--Crie uma visão que inclui mais de uma tabela. Faça uma inclusão na visão. Verifique o que acontece.
