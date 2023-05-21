bc275ee402115f6ad47de14463f1d4bfb1b07209
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-21 20:53:31 -0300

    Iniciando atividades em Java

---
+++ 2023/laboratorio_de_programacao/aula_12/exercicio_01/src/Circulo.java
@@ -0,0 +1,12 @@
+public class Circulo extends FormaGeometrica {
+
+    @Override
+    public void calcularArea() {
+
+    }
+
+    @Override
+    public void calcularPerimetro() {
+
+    }
+}
---
+++ 2023/laboratorio_de_programacao/aula_12/exercicio_01/src/FormaGeometrica.java
@@ -0,0 +1,8 @@
+public abstract class FormaGeometrica {
+    public void calcularArea(){
+        System.out.println("Calculando Área...");
+    }
+    public void calcularPerimetro(){
+        System.out.println("Calculando Perímetro...");
+    }
+}
