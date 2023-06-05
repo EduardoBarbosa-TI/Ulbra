7a9d58c60010560140857db039b0a9a39a0282fb
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-06-05 20:14:09 -0300

    fix: changed logic

--- 2023/laboratorio_de_programacao/aula_16/exercicio05/src/Main.java
+++ 2023/laboratorio_de_programacao/aula_16/exercicio05/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numerosImpares = {1,3,5,7,9,11,13,15,17,19};
+
+        int[] numerosImpares = new int[10];
+        for(int i = 0; i < numerosImpares.length;i++ ){
+            numerosImpares[i] = i * 2 - 1;
+        }
 
         for(int i = 0; i < numerosImpares.length; i++){
             if(numerosImpares[i] % 2 == 1){
