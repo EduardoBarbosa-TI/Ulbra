5c092c20c7d8049f5ec122e014dc5edfaa8f84f1
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-09-05 11:31:50 -0300

    modificado detalhes da calculadora

--- 2023/programacao_orientada_a_objetos/aula_03/tde/src/Calculadora.java
+++ 2023/programacao_orientada_a_objetos/aula_03/tde/src/Calculadora.java
@@ -3,7 +3,5 @@ abstract class Calculadora {
     private double result;
     abstract void calcular(IOperador operador, ArrayList<Double> lista);
     public double getResult() {return result;}
-    public void setResult(double result) {
-        this.result = result;
-    }
+    public void setResult(double result) {this.result = result;}
 }
