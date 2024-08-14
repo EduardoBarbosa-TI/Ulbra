86f218ec1b05e18f71f1dc3199d0b49a17313e48
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2024-08-14 20:03:04 -0300

    modified folders

--- 2024/desenvolvimento_aplicacao_OC/atividade_01.js
+++
@@ -1,6 +0,0 @@
-sum(10,30);
-
-function sum(numberA, numberB){
-    const sum = Number(numberA) + Number(numberB);
-    console.log(sum);
-}
--- 2024/desenvolvimento_aplicacao_OC/atividade_02.js
+++
@@ -1,8 +0,0 @@
-verifyNumbers([10,20,30,7,5]);
-
-function verifyNumbers(listNumber){
-    for(number of listNumber){
-        const result = number%2 == 0 ? "Par" : "Ímpar";
-        console.log("O número " + number + " é " + result);
-    }
-}
