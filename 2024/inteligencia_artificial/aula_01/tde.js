18016cf6466abe1e0b64bd292cab882a668a96e1
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2024-08-25 15:36:02 -0300

    fix: correction of nomenclature

--- 2024/inteligencia_artificial/aula_01/tde.js
+++ 2024/inteligencia_artificial/aula_01/tde.js
@@ -5,60 +5,60 @@ let input41 = 1, input42 = 1, result04 = 1
 
 let p1 = -1, p2 = -1
 
-let sum, ajustes = 0,quantityAjustesTotais = 0, repeticoes = 0
+let sum, settings = 0,totalQuantityAdjustments = 0, repetitions = 0
 
 do {
-    ajustes = 0
+    settings = 0
 
-    sum = somar(input11, input12)
-    y = tranferencia(sum)
+    sum = add(input11, input12)
+    y = transfer(sum)
     if(y != result01){
         ajustar(input11, input12, result01, y)
-        ajustes++
-        quantityAjustesTotais++
+        settings++
+        totalQuantityAdjustments++
     }
 
-    sum = somar(input21, input22)
-    y = tranferencia(sum)
+    sum = add(input21, input22)
+    y = transfer(sum)
     if(y != result02){
-        ajustar(input21, input22, result02, y)
-        ajustes++
-        quantityAjustesTotais++
+        adjust(input21, input22, result02, y)
+        settings++
+        totalQuantityAdjustments++
     }
 
-    sum = somar(input31, input32)
-    y = tranferencia(sum)
+    sum = add(input31, input32)
+    y = transfer(sum)
     if(y != result03){
-        ajustar(input31, input32, result03, y)
-        ajustes++
-        quantityAjustesTotais++
+        adjust(input31, input32, result03, y)
+        settings++
+        totalQuantityAdjustments++
     }
 
-    sum = somar(input41, input42)
-    y = tranferencia(sum)
+    sum = add(input41, input42)
+    y = transfer(sum)
     if(y != result04){
-        ajustar(input41, input42, result04, y)
-        ajustes++
-        quantityAjustesTotais++
+        adjust(input41, input42, result04, y)
+        settings++
+        totalQuantityAdjustments++
     }
 
-} while (ajustes != 0)
+} while (settings != 0)
 
 console.log("Teste de aprendizagem com rede neural")
 console.log("Peso 01 " + p1);
 console.log("Peso 02 " + p2);
-console.log("Foram necessarios " + quantityAjustesTotais + " ajustes para treinar a rede neural");
+console.log("Foram necessarios " + totalQuantityAdjustments + " settings para treinar a rede neural");
 
 
-function somar(input01, input02){
+function add(input01, input02){
     return (input01 * p1) + (input02 * p2)
 }
 
-function tranferencia(sum){
+function transfer(sum){
     return sum <= 0 ? 0 : 1; 
 }
 
-function ajustar(input01, input02, resultEsperado, resultObtido){
-    p1 = p1 + 1  * (resultEsperado - resultObtido) * input01
-    p2 = p2 + 1  * (resultEsperado - resultObtido) * input02 
+function adjust(input01, input02, expectedResult, resultObtained){
+    p1 = p1 + 1  * (expectedResult - resultObtained) * input01
+    p2 = p2 + 1  * (expectedResult - resultObtained) * input02 
 }
