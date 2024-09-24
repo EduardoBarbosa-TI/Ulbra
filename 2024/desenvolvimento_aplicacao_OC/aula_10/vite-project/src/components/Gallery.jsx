bcd6709c70b680ea7a8779e6f0f59bc3a26efdf5
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2024-09-23 22:02:39 -0300

    finished

---
+++ 2024/desenvolvimento_aplicacao_OC/aula_10/vite-project/src/components/Gallery.jsx
@@ -0,0 +1,14 @@
+import Profile from "./Profile"
+
+function Gallery(){
+    return (
+     <div>
+        <h1>Galeria do Edu</h1>
+        <Profile></Profile>
+        <Profile></Profile>
+        <Profile></Profile>
+     </div>
+    )
+}
+
+export default Gallery
